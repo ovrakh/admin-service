40068fda34fa3c6f058ed38c535e469d2cbe69dd
Алексей <notik-lexa@mail.ru>
2018-03-25 14:16:56 +0300

    add handler

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { RegService } from '../reg.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+    /*private regService: RegService*/) { }
 
   ngOnInit() {
+  }
+
+  add(name: string, password: string): void {
+    name = name.trim();
+    password = password.trim();
+    if (!name) { return; }
+    if (!password) { return; }
+    alert(name);
+    this.router.navigate(['/']);
+    // this.heroService.addHero({ name } as Hero)
+    //   .subscribe(hero => {
+    //     this.heroes.push(hero);
+    //   });
   }
 
 }
